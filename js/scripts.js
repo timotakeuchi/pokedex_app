9fbc7b3113c329dd27455f615a9bb64db3493996
timotakeuchi <117367638+timotakeuchi@users.noreply.github.com>
2023-03-16 20:27:12 -0400

    Update scripts.js
    Created an array of Pokemon, their height, and types listed in an nested object for the purpose of showcasing their various attributes.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name: 'Bulbasaur', height: '0.7 meters', type:['Grass', 'Poison']},
+    {name: 'Ivysaur', height: '1 meters', type:['Grass', 'Poison']},
+    {name: 'Venusaur', height: '2 meters', type:['Grass', 'Poison']}
+];
