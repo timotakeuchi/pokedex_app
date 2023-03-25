5cd39e181bf9a115183a79e926bcb2f80563a8ee
timotakeuchi <117367638+timotakeuchi@users.noreply.github.com>
2023-03-25 00:31:10 -0400

    Update scripts.js
    edited height of each Pokemon for easy readability for the loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: '0.7 meters', type:['Grass', 'Poison']},
-    {name: 'Ivysaur', height: '1 meters', type:['Grass', 'Poison']},
-    {name: 'Venusaur', height: '2 meters', type:['Grass', 'Poison']}
+    {name: 'Bulbasaur', height: 0.7, type:['Grass', 'Poison']},
+    {name: 'Ivysaur', height: 1, type:['Grass', 'Poison']},
+    {name: 'Venusaur', height: 2, type:['Grass', 'Poison']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height >= 2) {
+        document.write(`<li>${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!</li>`)
+    } else {
+        document.write(`<li>${pokemonList[i].name} (height: ${pokemonList[i].height})</li>`)
+    }
+}
