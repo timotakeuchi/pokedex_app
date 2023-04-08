172fe72a536307a242db3379a9dbf8dc59b1a2b6
timotakeuchi <117367638+timotakeuchi@users.noreply.github.com>
2023-04-08 16:28:57 -0400

    Update scripts.js
    Added getAll and add to IIFE for organizing the object

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
-let pokemonRepository = (function () { 
+// // let pokemonRepository = (function () { 
+// let pokemonList = [
+//     {
+//         name: 'Bulbasaur', 
+//         height: 0.7, 
+//         type:['Grass', 'Poison']
+//     },
+//     {
+//         name: 'Ivysaur', 
+//         height: 1, 
+//         type:['Grass', 'Poison']
+//     },
+//     {
+//         name: 'Venusaur', 
+//         height: 2, 
+//         type:['Grass', 'Poison']
+//     }
+// ];
+
+//   function getAll (pokemon) {
+//     return pokemonlist
+//   };
+
+//   function add (item) {
+//     return { name: 'Charmander', height: 0.6, type: 'fire'}
+//   }
+
+// })();
+
 let pokemonList = [
     {
         name: 'Bulbasaur', 
@@ -15,17 +43,6 @@ let pokemonList = [
         height: 2, 
         type:['Grass', 'Poison']
     }
-];
-
-  function getAll (pokemon) {
-    return pokemonlist
-  };
-
-  function add (item) {
-    return { name: 'Charmander', height: 0.6, type: 'fire'}
-  }
-
-})();
 
 // //Starting from 0, the loop references the pokemonList and sorts the data according to size.
 // for (let i = 0; i < pokemonList.length; i++){ 
@@ -38,6 +55,6 @@ let pokemonList = [
 //     }
 // }
 
-pokemonList.getAll.forEach (function(pokemon) {
+pokemonList.forEach(function(pokemon) {
     document.write('pokemonList.name ' + '(' + 'height:' + ' pokemonList.height')
 });
