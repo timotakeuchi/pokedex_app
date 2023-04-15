e8af6c62bbfd4aa8d85ea169d3a7efd575db66e7
timotakeuchi <117367638+timotakeuchi@users.noreply.github.com>
2023-04-15 19:34:55 -0400

    Update scripts.js
    Created a return in the pokemonRepository function which details the add and getAll functions. Added Charmandar to the array of objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,35 @@
-let pokemonList = [
-  {name: 'Bulbasaur', height: 0.7, type:['Grass', 'Poison']},
-  {name: 'Ivysaur', height: 1, type:['Grass', 'Poison']},
-  {name: 'Venusaur', height: 2, type:['Grass', 'Poison']}
-];
-
-// for (let i = 0; i < pokemonList.length; i++){
-//   if (pokemonList[i].height >= 2) {
-//       document.write(`<li>${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!</li>`)
-//   } else {
-//       document.write(`<li>${pokemonList[i].name} (height: ${pokemonList[i].height})</li>`)
-//   }
-// }
-
-pokemonList.forEach(function(pokemon){
-  if (pokemon.height >= 2) {
-      document.write(`<li>${pokemon.name} (height: ${pokemon.height}) - Wow, that's big!</li>`)
-  } else {
-      document.write(`<li>${pokemon.name} (height: ${pokemon.height})</li>`)}
-})
+const pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, type:['Grass', 'Poison']},
+    {name: 'Ivysaur', height: 1, type:['Grass', 'Poison']},
+    {name: 'Venusaur', height: 2, type:['Grass', 'Poison']}
+  ]
+  
+    return {
+      add: function(pokemon){
+       pokemonList.push(pokemon);
+    },
+      getAll: function(){
+       return pokemonList;
+    }
+   };
+  })();
+    
+  
+  
+  // for (let i = 0; i < pokemonList.length; i++){
+  //   if (pokemonList[i].height >= 2) {
+  //       document.write(`<li>${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!</li>`)
+  //   } else {
+  //       document.write(`<li>${pokemonList[i].name} (height: ${pokemonList[i].height})</li>`)
+  //   }
+  // }
+  pokemonRepository.add({name: 'Charmandar', height: 0.6, type: 'Fire'});
+  
+  pokemonRepository.getAll().forEach(function(pokemon){
+    if (pokemon.height >= 2) {
+        document.write(`<li>${pokemon.name} (height: ${pokemon.height}) - Wow, that's big!</li>`)
+    } else {
+        document.write(`<li>${pokemon.name} (height: ${pokemon.height})</li>`)}
+  });
+  
