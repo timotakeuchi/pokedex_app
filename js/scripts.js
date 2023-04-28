00504a0c948705c5e3ae86495a2252177428a1c3
timotakeuchi <117367638+timotakeuchi@users.noreply.github.com>
2023-04-28 01:14:29 -0400

    If/else, forEach & syntax corrections,
    Created an if/else statement for the purpose of creating an logging new Pokemon into the list.
    
    Corrected the syntax in "type" for the object added into the pokemonRepository.
    
    fixed the forEach method so that it can function better.
    
    Turned the if/else statement written below into a comment.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,54 @@
-const pokemonRepository = (function () {
+let pokemonRepository = (function () {
   let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, type:['Grass', 'Poison']},
     {name: 'Ivysaur', height: 1, type:['Grass', 'Poison']},
     {name: 'Venusaur', height: 2, type:['Grass', 'Poison']}
-  ]
+  ];
   
-    return {
-      add: function(pokemon){
-       pokemonList.push(pokemon);
-    },
-      getAll: function(){
-       return pokemonList;
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
     }
-   };
-  })();
-    
+    // pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
   
+  function addListItem (pokemon) { 
+   //list of variables in the function
+    let pokemonList = document.querySelector('.pokemon-list')
+    let pkListItem = document.createElement('li')
+    let button = document.createElement('button')
+    button.addEventListener('click', pokemon => showDetails(pokemon))
+  
+   //button features
+    button.innerText = pokemon.name;
+    button.classList.add('pkbutton');
+    
+   //appending children
+    pokemonList.appendChild(pkListItem);
+    pkListItem.appendChild(button);
+  }
+  
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem 
+  };
+})();
   
   // for (let i = 0; i < pokemonList.length; i++){
   //   if (pokemonList[i].height >= 2) {
@@ -24,20 +57,10 @@ const pokemonRepository = (function () {
   //       document.write(`<li>${pokemonList[i].name} (height: ${pokemonList[i].height})</li>`)
   //   }
   // }
-  pokemonRepository.add({name: 'Charmandar', height: 0.6, type: 'Fire'});
+  pokemonRepository.add({name: 'Charmandar', height: 0.6, types: 'Fire'});
   
-  pokemonRepository.getAll().forEach(function(pokemon){
+pokemonRepository.getAll().forEach(function(pokemon){pokemonRepository.addListItem(pokemon)});
     // if (pokemon.height >= 2) {
     //     document.write(`<li>${pokemon.name} (height: ${pokemon.height}) - Wow, that's big!</li>`)
     // } else {
     //     document.write(`<li>${pokemon.name} (height: ${pokemon.height})</li>`)}
-    let pokemonList = document.querySelector('.pokemon-list')
-    let pkListItem = document.createElement('li')
-    let button = document.createElement('button')
-    button.innerText('pokemon');
-    button.classList.add('.pkbutton')
-    pokemonList.firstChild.appendChild(button)
-  });
-
-  
-
