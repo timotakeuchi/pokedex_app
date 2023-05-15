ca79132dc1f1b0aae07929996d3971c43db2fbe7
timotakeuchi <117367638+timotakeuchi@users.noreply.github.com>
2023-05-15 01:27:51 -0400

    Loaded pokemon items from the API
    In order to load the list of Pokemon from the API, the function loadList() was created.

--- js/scripts.js
+++ js/scripts.js
@@ -34,6 +34,23 @@ let pokemonRepository = (function () {
     button.addEventListener('click', event => showDetails(pokemon));
   }
   
+  function loadList(){
+    return fetch(apiUrl).then(function(response){
+      return response.json();
+    }).then(function(json){
+      json.results.forEach(function(item){
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function(e){
+      console.error(e);
+    })
+  }
+
   return {
     add: add,
     getAll: getAll,
