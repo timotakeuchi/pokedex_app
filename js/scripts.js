1d14aecda6a798735eac622fbd4760b37a4e4a9f
timotakeuchi <117367638+timotakeuchi@users.noreply.github.com>
2023-05-15 02:16:45 -0400

    Added loadDetails()
    For the purpose of adding the details to each of the Pokemon, the promise loadDetails() was created.

--- js/scripts.js
+++ js/scripts.js
@@ -51,6 +51,21 @@ let pokemonRepository = (function () {
     })
   }
 
+  function loadDetails(item){
+    let url = item.detailsUrl;
+    return fetch(url).then(function(response){
+      return response.json();
+    }).then(function(details){
+      item.image = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function(e){
+      console.error(e);
+    });
+  }
+
+  
+
   return {
     add: add,
     getAll: getAll,
